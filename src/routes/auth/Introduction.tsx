40bc47f3f3e8973d17fc4ab7083a309670778960
Mac <mac@192.168.1.4>
2024-09-16 00:15:06 +0700

    feat: add animation for block introduction

--- src/routes/auth/Introduction.tsx
+++ src/routes/auth/Introduction.tsx
@@ -5,41 +5,55 @@ const PageWrapper = styled.div`
     background-color: ${theme.colors.primaryLight};
     padding: 20px;
 `
-const moveAround = keyframes`
-    10%, 90% {
+const moveAround= (index: number) => keyframes`
+    0%, 20% {
         transform: translate(0, 0);
     }
-    20%, 30% {
-        transform: translate(0, 100%);
+    25%, 45% {
+        transform: translate(${index % 3 === 0 ? 0 : index % 2 === 0 ? "100%" : "-100%"}, ${index % 3 !== 0 ? 0 : index !== 0 ? "-100%" : "100%" });
     }
-    40%, 60% {
-        transform: translate(100%, 100%);
+    50%, 70% {
+        transform: translate(${index % 2 === 0 ? "100%" : "-100%"}, ${index >= 2 ? '-100%' : "100%" });
     }
-    70%, 80%{
-        transform: translate(100%, 0);
+    75%, 95%{
+        transform: translate(${index % 3 !== 0 ? 0 : index !== 0 ? "-100%" : "100%"}, ${index % 3 !== 0 ? index % 2 === 0 ? "-100%" : "100%" : 0});
     }
 `;
+
+// const moveRules = [
+//     {x1:0,y1:100, x2:100, y2:100, x3:100, y3:0},
+//     [{x:-100,y:0}, {x:-100, y:100}, {x:0, y:100}],
+//     [{x:100, y:0}, {x:100, y:-100}, {x:0, y:-100}],
+//     [{x:0,y:-100}, {x:-100, y:-100}, {x:-100, y:0}],
+// ]
 
 const Blocks = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
-    gap: 20px;   
 `
 
-const Block = styled.span<{backgroundColor: string}>`
-    background-color: ${props => props.backgroundColor};
+const Block = styled.span<{backgroundColor: string, index: number}>`
     aspect-ratio: 1/1;
-    border-radius: 12px;
-    animation: ${moveAround} 10s linear infinite;
+    padding: 20px;
+    animation: ${props => moveAround(props.index)} 10s linear infinite;
+    >div {
+        background-color: ${props => props.backgroundColor};
+        width: 100%;
+        height: 100%;
+        border-radius: 10%;
+        color: ${theme.colors.primaryText};
+    }
 `
 const Component =() => {
     return (
         <PageWrapper>
             <Blocks>
-                {Array(1).fill("").map((_, index) => {
+                {Array(4).fill("").map((_, index) => {
                     const color = (index % 3) === 0 ? theme.colors.primaryDark : theme.colors.primaryMedium;
                     return (
-                        <Block backgroundColor={color}/>
+                        <Block backgroundColor={color} index={index}>
+                            <div>Guest</div>
+                        </Block>
                     );
                 })}
             </Blocks>
